35b129cac18fe9e3785ec986409a2a3c97c1bc38
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:39:41 -0400

    set up annotations for book

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -28,6 +28,14 @@ public class Author {
         this.books = books;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     //set up getters and setters
     public String getFirstName() {
         return firstName;
