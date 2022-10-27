a0620fb535e11e7ae45e7adece5f9a40548528e4
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:38:06 -0400

    set up annotations for author

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,8 +1,17 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity //annotate with entity to tell hibernate that this is an entity and make a primary key
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //automatic id generation
+    private Long id;
     private String firstName;
     private String lastName;
     private Set<Book> books;
