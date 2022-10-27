94c22bfff5364bbfa6e9a5ec222a9e0191a8ec77
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:28:18 -0400

    set private instances

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+
+
+}
