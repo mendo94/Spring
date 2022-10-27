1536f44895776271f0e6c6e16ae046b90fbde47b
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:29:25 -0400

    added constructor

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -7,6 +7,10 @@ public class Author {
     private String lastName;
     private Set<Book> books;
 
-
-
+        // added in constructor with  cmd constructor all
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
 }
