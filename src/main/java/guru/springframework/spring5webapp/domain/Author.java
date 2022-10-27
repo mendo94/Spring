9cca660daa369b8c1e117d6fab9061694917a978
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:31:36 -0400

    set up getters and setters

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -7,10 +7,40 @@ public class Author {
     private String lastName;
     private Set<Book> books;
 
-        // added in constructor with  cmd constructor all
+    public Author() {
+        //JPA requires a zero args constructor
+
+    }
+
+        // added in constructor with  cmd+n constructor all
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.books = books;
+    }
+
+    //set up getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
         this.books = books;
     }
 }
