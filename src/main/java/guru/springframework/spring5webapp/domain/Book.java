ce19474332e4aef005bf3c9a91d2d26c16c58d0f
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-26 21:34:50 -0400

    set up getters and setters

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,4 +16,28 @@ public class Book {
         this.isbn = isbn;
         this.authors = authors;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
